e636689cebd7df32ca5df7435fc322dcc18f960f
strabel2 <61198911+strabel2@users.noreply.github.com>
2020-12-03 18:56:45 +0100

    Add files via upload

---
+++ app.R
@@ -0,0 +1,141 @@
+# libraries
+library(shiny)
+library(dplyr)
+library(shinydashboard)
+library(DT)
+library(tidyverse)
+library(plotly)
+library("jsonlite")
+
+
+url_req <- "https://coronavirusapi-france.now.sh/AllLiveData"
+book_data <- fromJSON(url_req)
+data<-book_data$allLiveFranceData
+
+
+# ui ----------------------------------------------------------------------
+
+
+## app.R ##
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+    dashboardHeader(title="Dashboard Covid"),
+    
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Onglet Situation", tabName = "situation",icon=icon("situation")),
+            menuItem("Onglet Historique", tabName = "historique",icon = icon("historique"))),
+        
+        # Choix du département 
+        selectInput("departement",
+                    "Choisissez votre departement:",
+                    choices = unique(data$nom),
+                    selected = 'Doubs'),
+        
+        
+        #Choix de la date
+        selectInput("date","Choix de la date",choices=unique(data$date),selected = '2020-11-26'),
+        
+        ##action button 
+        #actionButton(inputId = 'action','Lancer l analyse'),
+        
+        
+        downloadLink('downloadData', 'Telecharger')
+        
+        
+        
+        
+    ),
+    
+    
+    
+    dashboardBody(
+        fluidRow(
+            
+            # Dynamic valueBoxes
+            valueBoxOutput("valuebox1")
+            
+        ),
+        
+        tabItems(
+            #Premier onglet : mon departement
+          
+            
+            tabItem('situation')),
+            
+            
+            
+            
+            
+            tabItems(
+                #Premier onglet : mon departement
+                tabItem('historique'))
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        ),
+        
+        skin="purple"
+        
+    )
+
+server <- function(input, output){
+    
+    output$downloadData <- downloadHandler(
+        filename = function() {
+            paste('data-', Sys.Date(), '.csv', sep=",")
+        },
+        content = function(con) {
+            write.csv(data, con)
+        }
+    )  
+    
+    
+    
+    output$valuebox1<-renderValueBox({
+        ####choisir le departement 
+        valueBox(value=paste(sum(data$hospitalises)),"Hospitalisés",
+                 icon = icon("list"),color = "blue")
+        
+        
+    }
+    
+    
+    
+    
+    
+    )
+    
+    
+    
+    
+    
+    
+    
+    
+}    
+
+
+
+
+
+
+
+
+shinyApp(ui, server)
