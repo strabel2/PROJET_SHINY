0f3c88047dcfe9f9c5d3d3ed5b4fe13263433447
strabel2 <61198911+strabel2@users.noreply.github.com>
2020-12-09 10:54:36 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -6,11 +6,15 @@ library(DT)
 library(tidyverse)
 library(plotly)
 library("jsonlite")
+library(ggplot2)
+library(dplyr)
+library(lubridate)
 
 
-url_req <- "https://coronavirusapi-france.now.sh/AllLiveData"
-book_data <- fromJSON(url_req)
-data<-book_data$allLiveFranceData
+url="https://coronavirusapi-france.now.sh/AllDataByDepartement?Departement=Rhône"
+book_data <- fromJSON(url)
+
+data=book_data$allDataByDepartement
 
 
 # ui ----------------------------------------------------------------------
@@ -27,24 +31,28 @@ ui <- dashboardPage(
         sidebarMenu(
             menuItem("Onglet Situation", tabName = "situation",icon=icon("situation")),
             menuItem("Onglet Historique", tabName = "historique",icon = icon("historique"))),
-        
-        # Choix du département 
-        selectInput("departement",
-                    "Choisissez votre departement:",
-                    choices = unique(data$nom),
-                    selected = 'Doubs'),
+    
         
         
         #Choix de la date
         selectInput("date","Choix de la date",choices=unique(data$date),selected = '2020-11-26'),
         
- 
+        
         
         ##action button 
         #actionButton(inputId = 'action','Lancer l analyse'),
         
         
-        downloadLink('downloadData', 'Telecharger')
+        downloadLink('downloadData', 'Telecharger'),
+        
+        
+        #########2 eme onglet########"
+        
+        #Choix de la date
+        selectInput("date_debut","Choix de la date de début",choices=unique(data$date),selected = '2020-11-26'),
+        #Choix de la date
+        selectInput("date_fin","Choix de la date de fin",choices=unique(data$date),selected = '2020-11-26')
+        
         
         
         
@@ -54,58 +62,55 @@ ui <- dashboardPage(
     
     
     dashboardBody(
-        fluidRow(
-            
-            # Dynamic valueBoxes
-            valueBoxOutput("valuebox1")
-            
-        ),
+    
         
         tabItems(
             #Premier onglet : mon departement
-          
-            
-            tabItem('situation')
             
             
-            
-            
-            ),
-            
-            
-            
-            
-            
-            tabItems(
-                #Premier onglet : mon departement
-                tabItem('historique'))
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+            tabItem('situation',class='dynamic',valueBoxOutput("valuebox1"),
+                    valueBoxOutput("valuebox2"),
+                    valueBoxOutput("valuebox3"))
             
             
             
             
         ),
         
-        skin="purple"
         
-    )
+        
+        
+        
+        tabItems(
+            #Premier onglet : mon departement
+            tabItem('historique',plotlyOutput('gueris'))
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    ),
+    
+    skin="purple"
+    
+))
 
 server <- function(input, output){
-
-  
-
+    
+    
+    
     
     output$downloadData <- downloadHandler(
         filename = function() {
@@ -119,20 +124,43 @@ server <- function(input, output){
     
     
     output$valuebox1<-renderValueBox({
-      hospitalises=data%>%filter(data$nom==input$departement & data$date==input$date)
-      
-      valueBox(value=paste(hospitalises$hospitalises),"Hospitalisés",
+        hospitalises=data%>%filter(data$date==input$date)
+        
+        valueBox(value=paste(hospitalises$hospitalises),"Hospitalisés",
                  icon = icon("stethoscope"),color = "blue")
         
         
-    }
+    })
+    
+    output$valuebox2<-renderValueBox({
+        hospitalises=data%>%filter(data$date==input$date)
+        
+        valueBox(value=paste(hospitalises$casConfirmes),"Cas Confirmés",
+                 icon = icon("stethoscope"),color = "purple")
+        
+        
+    })
+    
+    output$valuebox3<-renderValueBox({
+        hospitalises=data%>%filter(data$date==input$date)
+        
+        valueBox(value=paste(hospitalises$nouvellesReanimations),"Nouvelles Réanimations",
+                 icon = icon("stethoscope"),color = "orange")
+        
+        
+    })
     
     
-    
-    
-    
-    )
-    
+    output$gueris <- renderPlotly({
+        
+        d=data%>%filter(data$date <=input$date_fin & data$date >=input$date_debut)
+        Date=as.Date(d$date)
+        df=ggplot(d, aes(x=Date, y=gueris)) + 
+            geom_point(color="magenta")+scale_x_date(date_labels = "%Y %b %d")
+        out=ggplotly(df,width = 700,height = 500)
+        out
+        
+    }) 
     
     
     
